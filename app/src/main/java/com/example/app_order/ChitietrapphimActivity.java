291ec5dc8988ab2660c94e04e273e75030585f1a
KhuongNganHuyen <khuongthuonghuyen0@gmail.com>
2024-10-13 04:15:07 +0700

    chi tiet rap phim

--- app/src/main/java/com/example/app_order/ChitietrapphimActivity.java
+++ app/src/main/java/com/example/app_order/ChitietrapphimActivity.java
@@ -4,11 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ChitietrapphimActivity extends AppCompatActivity {
 
