38f57b1bb2d540691e23759b682acb8ab767d842
DoThiQuynhNhung <dothiquynhnhung12.2@gmail.com>
2024-12-02 12:38:40 +0700

    Merge remote-tracking branch 'origin/QuynhNhung' into QuynhNhung

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -52,4 +53,7 @@ dependencies {
     implementation ("com.codesgood:justifiedtextview:1.1.0")
     implementation ("me.relex:circleindicator:2.1.6")
     implementation ("androidx.recyclerview:recyclerview:1.2.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
